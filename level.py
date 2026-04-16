69ca4b82cceee2254615282c085a7a6e7f49a6dc
awoolf07 <awoolf07@gmail.com>
2026-04-16 01:01:55 -0400

    Add docstrings for init and its helper functions in Level

--- level.py
+++ level.py
@@ -27,4 +27,41 @@ class Level():
         Args:
             path: A string representing the path of the folder.
         """
+
+    @classmethod
+    def read_variable(cls, text, name):
+        """
+        Find the value of a variable as defined in a string.
+
+        Args:
+            text: A string representing a set of variable names and
+                  the values associated with them.
+            name: The name of the variable to find the value of.
         
+        Returns:
+            A string representing the value of the variable.
+        """
+
+    @classmethod
+    def read_list(cls, text):
+        """
+        Find the elements in a list described by a string.
+
+        Args:
+            test: A string representing a list of values.
+        
+        Returns:
+            A list representing the values described in text.
+        """
+
+    @classmethod
+    def read_vector(cls, text):
+        """
+        Find the Vector described by a string.
+
+        Args:
+            text: A string representing a vector.
+        
+        Returns:
+            The Vector described by text.
+        """
