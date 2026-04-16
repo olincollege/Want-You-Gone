9861deea3368dfda98d93a6bcd01556769fca10d
awoolf07 <awoolf07@gmail.com>
2026-04-16 14:25:09 -0400

    Initialize player and boarder

--- level.py
+++ level.py
@@ -2,6 +2,8 @@
 Contains the Level class.
 """
 
+from shape import Circle, Polygon
+
 class Level():
     """
     Store the physical state of a set of 2D shapes.
@@ -27,6 +29,65 @@ class Level():
         Args:
             path: A string representing the path of the folder.
         """
+        # Read the file for player.
+        with open(path + "player.txt", "r", encoding="utf-8") as f:
+            player_text = f.read()
+
+        # Extract the attributes of player.
+        player_position = self.read_vector(
+            self.read_variable(player_text, "position"))
+        player_velocity = self.read_vector(
+            self.read_variable(player_text, "velocity"))
+        player_angle = float(self.read_variable(player_text, "angle"))
+        player_angular_velocity = float(
+            self.read_variable(player_text, "angular_velocity"))
+        player_can_move = True
+        player_is_inverted = False
+        player_is_bouncy = self.read_boolean(
+            self.read_variable(player_text, "is_bouncy"))
+        player_radius = abs(float(self.read_variable(player_text, "radius")))
+
+        # Initialize player.
+        self._player = Circle(player_position,
+                              player_velocity,
+                              player_angle,
+                              player_angular_velocity,
+                              player_can_move,
+                              player_is_inverted,
+                              player_is_bouncy,
+                              player_radius)
+
+        # ----------------------------------------------------------------------
+
+        # Read the file for boarder.
+        with open(path + "boarder.txt", "r", encoding="utf-8") as f:
+            boarder_text = f.read()
+
+        # Extract the attributes of boarder.
+        player_position = self.read_vector(
+            self.read_variable(boarder_text, "position"))
+        player_velocity = self.read_vector(
+            self.read_variable(boarder_text, "velocity"))
+        player_angle = float(self.read_variable(boarder_text, "angle"))
+        player_angular_velocity = float(self.read_variable(
+            boarder_text, "angular_velocity"))
+        player_can_move = False
+        player_is_inverted = True
+        player_is_bouncy = self.read_boolean(self.read_variable(
+            boarder_text, "is_bouncy"))
+        player_vertices = [self.read_vector(vertex)
+                           for vertex in self.read_list(
+                           self.read_variable(boarder_text, "vertices"))]
+
+        # Initialize boarder.
+        self._boarder = Polygon(player_vertices,
+                         player_position,
+                         player_velocity,
+                         player_angle,
+                         player_angular_velocity,
+                         player_can_move,
+                         player_is_inverted,
+                         player_is_bouncy)
 
     @classmethod
     def read_variable(cls, text, name):
@@ -40,6 +101,9 @@ class Level():
         
         Returns:
             A string representing the value of the variable.
+
+        Raises:
+            ValueError: If variable is not defined in text.
         """
 
     @classmethod
@@ -64,4 +128,28 @@ class Level():
         
         Returns:
             The Vector described by text.
+        
+        Raises:
+            ValueError: If the vector doesn't have two indices
+            or if any of its indices can't be cast as floats.
         """
+
+    @classmethod
+    def read_boolean(cls, text):
+        """
+        Find the boolean described by a string.
+
+        Args:
+            text: A string representing a boolean.
+        
+        Returns:
+            The boolean described by text.
+
+        Raises:
+            ValueError: If text is neither "True" nor "False".
+        """
+        if text == "True":
+            return True
+        if text == "False":
+            return False
+        raise ValueError("value is neither \"True\" nor \"False\"")
